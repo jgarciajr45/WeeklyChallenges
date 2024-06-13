f0282327542656cf802fbacb9321bc89c6a9b597
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-13 15:10:51 -0700

    challenge set 2 progress

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -7,41 +7,92 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
+            if ((c >= 'A' && c <= 'Z'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
             throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
+            //I DONT KNOW THIS ONE YET
             throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
+            int length1 = str1.Length;
+            int length2 = str2.Length;
+            return Math.Min(length1, length2);
             throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total += number;
+            }
+            return total;
             throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
+            int total = 0;
+            foreach (int number in numbers)  //STOPPED HERE??????
+            {
+                if (number % 2 == 0)
+                {
+                    total += number;
+                }
+                return total;
+            }
             throw new NotImplementedException();
         }
 
