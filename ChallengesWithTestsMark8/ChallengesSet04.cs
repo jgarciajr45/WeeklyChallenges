639b72c25e6eb1c521e71de96c52f1b230266e72
Jesus Garcia <jgarciajr45@gmail.com>
2024-07-12 17:39:08 -0700

    most of set5

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,135 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    result += number;
+                }
+                else
+                {
+                    result -= number;
+                }
+            }
+
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int Length1 = str1.Length;
+            int length2 = str2.Length;
+            int length3 = str3.Length;
+            int length4 = str4.Length;
+
+            int shortestLength = Math.Min(Math.Min(Length1, length2), Math.Min(length3, length4));
+
+            return shortestLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int smallest = number1;
+            if (number2 < smallest)
+            {
+                smallest = number2;
+            }
+            if (number3 < smallest)
+            {
+                smallest = number3;
+            }
+            if (number4 < smallest)
+            {
+                smallest = number4;
+            }
+            return smallest;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 <= 0 || sideLength2 <= 0 || sideLength3 <= 0)
+            {
+                return false;
+            }
+
+            if (sideLength1 + sideLength2 > sideLength3 &&
+                sideLength1 + sideLength3 > sideLength2 &&
+                sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            bool isNumber = double.TryParse(input, out _);
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int countNull = 0;
+            int countNotNull = 0;
+
+            foreach (var obj in objs)
+            {
+                if (obj == null)
+                {
+                    countNull++;
+                }
+                else
+                {
+                    countNotNull++;
+                }
+            }
+            return countNull > countNotNull;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToArray();
+
+            if (evenNumbers.Length == 0)
+            {
+                return 0;
+            }
+
+            double average = evenNumbers.Average();
+
+            return average;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            int result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+
         }
+
+        
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +8,138 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int nextNumber = startNumber + 1;
+
+            while (nextNumber % n != 0)
+            {
+                nextNumber++;
+            }
+
+            return nextNumber;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            if (businesses == null || businesses.Length == 0)
+            {
+                return;
+            }
+            foreach (var business in businesses)
+            {
+                if (business.TotalRevenue == 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
+
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length < 2)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length < 2)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i + 1];
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            StringBuilder sentenceBuilder = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    if (sentenceBuilder.Length > 0)
+                    {
+                        sentenceBuilder.Append(" ");
+                    }
+                    sentenceBuilder.Append(word.Trim());
+                }
+            }
+
+            string sentence = sentenceBuilder.ToString();
+            if (sentence.Length > 0)
+            {
+                sentence += ".";
+            }
+
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0)
+            {
+                return new double[0];
+            }
+
+            List<double> everyFourthElement = new List<double>();
+
+            for (int i = 3; i < elements.Count; i += 4)
+            {
+                everyFourthElement.Add(elements[i]);
+            }
+
+            return everyFourthElement.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            if (nums == null || nums.Length < 2)
+            {
+                return false;
+            }
+
+            HashSet<int> seenNumbers = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                int complement = targetNumber - num;
+
+                if (seenNumbers.Contains(complement))
+                {
+                    return true;
+                }
+
+                seenNumbers.Add(num);
+            }
+
+            return false;
         }
     }
 }
